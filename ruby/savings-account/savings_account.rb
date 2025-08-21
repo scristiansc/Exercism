74cbff3334208b744471d85d4fb8385449fc07e5
scristiansc <scristian1012@gmail.com>
2025-08-21 17:11:36 -0500

    Finished saving-account exercism

--- ruby/savings-account/savings_account.rb
+++ ruby/savings-account/savings_account.rb
@@ -17,10 +17,18 @@ module SavingsAccount
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    balance + (balance * interest_rate(balance) / 100)
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    years = 0
+    balance = current_balance
+
+    while balance < desired_balance
+      balance = annual_balance_update(balance)
+      years += 1
+    end
+
+    years
   end
 end
